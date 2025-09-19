19c4f3873c195b8f56896c5e2361fde43afda39c
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-19 15:54:07 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace StreamReaderOpenText3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\file1.txt";
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+        }
+    }
+}
